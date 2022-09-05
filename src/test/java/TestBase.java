3127f58581280959dbd919fe67f1e11c4663b1d0
Mikhail Lugovskiy <mikhail.lugovskiy@gmail.com>
2022-09-06 00:09:58 +0300

    Initial commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setUp(@Optional("Chrome") String browser) {
+        switch (browser) {
+            case "Firefox" -> driver = new FirefoxDriver();
+            case "Chrome" -> driver = new ChromeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
